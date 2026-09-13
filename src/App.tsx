a2c9cb3b1d1e9ee26d14d6618edbb2f059b4cdd5
tahsinmesbhaturzo-blip <tahsinmesbhaturzo@gmail.com>
2026-09-13 21:42:18 +0600

    readme file created

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-// import { useState,Suspense } from "react"
+import { useState,Suspense } from "react"
 import Banner from "./components/Banner"
 import Footer from "./components/Footer"
 import Nav from "./components/Nav"
-// import Technologies from "./components/Technologies"
+import Technologies from "./components/Technologies"
 
-// const TechnologiesFetch = async():Promise<Technologies[]> => {
-//   const res= await fetch('/data.json')
-//   const data = await res.json()
-//   return data
-// }
+const TechnologiesFetch = async():Promise<Technologies[]> => {
+  const res= await fetch('/data.json')
+  const data = await res.json()
+  return data
+}
 
 function App() {
-  //  const [TechnologiesPromise] = useState(()=> TechnologiesFetch())
+   const [TechnologiesPromise] = useState(()=> TechnologiesFetch())
   return (
     <>
       <Nav />
       <Banner />
-      {/* <Suspense fallback={<h1>Loading the Technologies</h1>}>
+      <Suspense fallback={<h1>Loading the Technologies</h1>}>
       <Technologies TechnologiesPromise ={TechnologiesPromise}/>
-      </Suspense> */}
+      </Suspense>
       <Footer />
     </>
   )
 }
-
 export default App
--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,16 +1,16 @@
-// import type { TechnologiesType } from "./Type";
-// import use from "react"
-// interface Technologies{
-// TechnologiesPromise:Promise<TechnologiesType[]>
-// }
-// const Technologies = ({TechnologiesPromise}:Technologies) => {
-//     const Pera = use(TechnologiesPromise)
-//     console.log(Pera)
-//     return (
-//         <div>
+import type { TechnologiesType } from "./Type";
+import use, { useState } from "react"
+interface Technologies{
+TechnologiesPromise:Promise<TechnologiesType[]>
+}
+const Technologies = ({TechnologiesPromise}:Technologies) => {
+    const Pera = use(TechnologiesPromise)
+    console.log(Pera)
+    return (
+        <div>
             
-//         </div>
-//     );
-// };
+        </div>
+    );
+};
 
-// export default Technologies ;
+export default Technologies ;
